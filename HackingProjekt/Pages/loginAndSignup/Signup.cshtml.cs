8ff6e2881fbd579818c60d22200b2fa645fd6b10
Oliver O <Oliver@olskaer.dk>
2023-11-17 06:35:50 +0100

    Add project files.

---
+++ HackingProjekt/Pages/loginAndSignup/Signup.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HackingProjekt.Pages.NewFolder
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ HackingProjekt/modelLib/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HackingProjekt.modelLib
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string userName{ get; set; }
+  
+        public string  Password { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
